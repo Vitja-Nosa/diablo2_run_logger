5f51ce5591d569b049ca68dde44f69e9a6a78607
Vitja-Nosa <vitjanosa5@hotmail.com>
2024-04-18 02:33:07 +0200

    wip

--- Database.py
+++ Database.py
@@ -7,7 +7,7 @@ class Database(TinyDB):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.COLS = ['date', 'runName', 'itemName', 'soldFor']
-        self.protoDB = self.all()
+        self.changes = False
 
     def DbSearch(self, query):
         Table = Query()
@@ -17,26 +17,7 @@ class Database(TinyDB):
         results += self.search(Table.soldFor.matches(query, flags=re.IGNORECASE))
         return results
 
-    #this function get called whenever tableWidget item gets changed
-    def updateItem(self, item):
-        print('updating item')
-        # doc_id begins at 1 and not 0 so I +1
-        id = item.row()+1
-        new_value = item.text()
-        self.update({self.COLS[item.column()]: new_value} ,doc_ids=[id])
-        return new_value
-
-    def createRow(self, runName, itemName, soldFor):
-        date = datetime.now()    
-        date = date.strftime('%d/%m/%Y %H:%M')
-
-        row = {
-            "date": date,
-            "runName": runName, 
-            "itemName": itemName,
-            "soldFor": soldFor
-        }
-        self.insert(row)
-
-    def deleteRows(self):
-        print('test')
+    def DbSave(self, data):
+        self.changes = False
+        self.truncate()
+        self.insert_multiple(data)
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from PyQt5.QtCore import Qt, QEvent
 from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QDialog, QApplication, QMainWindow, QTableWidgetItem
+from PyQt5.QtWidgets import QDialog, QApplication, QMainWindow, QTableWidgetItem, QAction
 from PyQt5.uic import loadUi
 from datetime import datetime
 from Database import Database
@@ -16,8 +16,11 @@ class Window(QMainWindow):
         #events for elements
         self.tableWidget.installEventFilter(self)
         self.addLogBtn.clicked.connect(self.addLog)
-        self.tableWidget.itemChanged.connect(db.updateItem)  
-        # rowsRemoved.connect(self.rows_removed) this is a listener for when a row is removed
+
+        #shortcuts
+        save_action = QAction('Shortcut', self)
+        save_action.setShortcut('Ctrl+S')
+        save_action.triggered.connect(self.saveTable)
 
     def addLog(self):
         next_row = self.tableWidget.rowCount()
@@ -27,22 +30,15 @@ class Window(QMainWindow):
         date = date.strftime('%d/%m/%Y %H:%M')
         values = [date, self.runNameEl.text(), self.itemNameEl.text(), self.soldForEl.text()]
 
-        row = {}
-        if len(values) != len(db.COLS):
-            return False
-        for i, col in enumerate(db.COLS): 
-            row[col] = values[i] 
-        db.insert(row)
-
         for col, value in enumerate(values):
             item = QTableWidgetItem(value)
             self.tableWidget.setItem(next_row, col, item)
-    
+
     def loadData(self, search = None):
         if search is not None:
             results = db.DbSearch(search)
         else:
-            results = db.all()    
+            results = db.all()
 
         self.tableWidget.setRowCount(len(results))
         i = 0
@@ -54,7 +50,22 @@ class Window(QMainWindow):
             self.tableWidget.setItem(i, 2, QtWidgets.QTableWidgetItem(row["itemName"]))
             self.tableWidget.setItem(i, 3, QtWidgets.QTableWidgetItem(row["soldFor"]))
             i += 1
+
+    def tableToObj(self):
+        data = []
+        row_count = self.tableWidget.rowCount()
+        for row in range(row_count):
+            rowData = {}
+            for i, col in enumerate(db.COLS):
+                item = self.tableWidget.item(row, i)
+                rowData[col] = item.text()
+            data.append(rowData)
+        return data
     
+    def saveTable(self):
+        data = self.tableToObj
+        db.DbSave(data)
+
     def eventFilter(self, obj, event):
         if obj == self.tableWidget and event.type() == QEvent.KeyPress:
             key = event.key()
@@ -74,19 +85,21 @@ class Window(QMainWindow):
                         if len(map[key]) == len(db.COLS):
                             #delete the entire row
                             self.tableWidget.removeRow(key)
-                            print(key)    
-                            db.remove(doc_ids=[3])
                         else:
                             #empty item
                             for col in map[key]:
                                 if db.COLS[col] != 'date':
                                     new_item = QtWidgets.QTableWidgetItem('')
                                     self.tableWidget.setItem(key, col, new_item)
-
-                    print(map)            
-
-                #delete rows here
+            elif key == 83:
+                data = self.tableToObj()
+                db.DbSave(data)
+                print('data saved')
             return True
+
+        def closeEvent(self, event):
+            print('hello')
+            event.ignore()
 
         return super().eventFilter(obj, event)
 
@@ -94,6 +107,7 @@ app = QApplication(sys.argv)
 window = Window()
 
 widget = QtWidgets.QStackedWidget() 
+widget.setWindowTitle('Runner Log')
 widget.addWidget(window)
 widget.setFixedHeight(400)
 widget.setFixedWidth(1000)
